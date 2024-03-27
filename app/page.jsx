451db7d7329751061a61b4e3e33009d4d357746d
Jake <kkcoms@byui.edu>
2024-03-26 23:40:59 -0700

    removing chatwidget

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,15 @@
 import Sandbox from "./sandbox"
-import ChatbotWidget from "./chatbotWidget"
 
-// export const metadata = {
-//     title: process.env.siteTitle,
-//     description: 'A sample React app built using Next.JS powered by OpenAI API',
-//     viewport: 'maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0, width=device-width, user-scalable=0',
-//     icons: {
-//         icon: '/logo192.png',
-//         shortcut: '/logo192.png',
-//     }
-// }
+export const metadata = {
+    title: process.env.siteTitle,
+    description: 'A sample React app built using Next.JS powered by OpenAI API',
+    viewport: 'maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0, width=device-width, user-scalable=0',
+    icons: {
+        icon: '/logo192.png',
+        shortcut: '/logo192.png',
+    }
+}
 
 export default function Page() {
-    return       <ChatbotWidget />
-
+    return <Sandbox />
 }
